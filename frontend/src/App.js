85f0e93c35d9c0caf08a9388fbc0550ea066199c
meafs <abrar.fahim@yandex.com>
2021-06-02 20:37:49 +0600

    Add footer, material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import Footer from "./components/Footer/Footer"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
         </Switch>
+        <Footer/>
       </Router>
     </div>
   );
